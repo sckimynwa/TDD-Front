fd9bcaf31d48dbf3ef1651af5ac8aaba97f25d0b
Kim Sang Chul <tsb03672@naver.com>
2020-12-20 23:34:59 +0900

    [Setting] Package & Provider added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import ListContainer from "./Components/ListContainer";
+import TitlePresenter from "./Components/TitlePresenter";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <TitlePresenter />
+      <ListContainer />
+    </>
+  );
 }
 
 export default App;
